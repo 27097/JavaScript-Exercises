faed50bea1e1af2925fb3e2840cb59f568828cd8
27097 <tapiasgroup@gmail.com>
2024-03-25 13:00:08 -0300

    04

--- 04-MultidimensionalSumArray/MultidimensionalSumArray.js
+++ 04-MultidimensionalSumArray/MultidimensionalSumArray.js
@@ -1,9 +1,17 @@
 function mdArraySum(arr) {
-    // Your code here:
-    let arrays = arr.flat();
-    console.log(arrays);
+    // // Your code here: forma 1
+    // let arrays = arr.flat(6)
+    // let suma = 0;
+    // arrays.forEach(element => {
+    //     suma += element
+    // });
+    // return suma
+   
+   
+    // Your code here: forma 2
+//    arr.reduce((total, item)=> total + (Array.isArray(item) ? mdArraySum(item) )  )
 
 
 }
-
+console.log(mdArraySum( [ 2, [3,4], [5], [-3, [6 , [ 4,5 ] ] ] ]));
 module.exports = mdArraySum;
