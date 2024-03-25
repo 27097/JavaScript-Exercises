feda28bd227a8bad0c74601577fd8fdaca7722be
27097 <tapiasgroup@gmail.com>
2024-03-25 12:32:46 -0300

    02

--- 01-FindNeedle/FindNeedle.js
+++ 01-FindNeedle/FindNeedle.js
@@ -1,11 +1,12 @@
 function FindNeedle(haystack, needle) {
     // iterar sobre el haystack
     for (let i = 0; i < haystack.length; i += 1) {
-      // si la substring matchea el neddle podemos devoler el valor de i
+    
       if(haystack[i]===needle[0]){
         if (haystack.slice(i, i + needle.length) === needle) return i
       }
     }
+    
     return -1;
   }
 
--- 02-SumArray/SumArray.js
+++ 02-SumArray/SumArray.js
@@ -1,6 +1,13 @@
 function SumArray(arr, n) {
     // Your code here:
-
+    for (let i = 0; i < arr.length; i++) {
+           for (let j = 0; j < arr.length; j++) {
+               if(arr[i] + arr[j] === n) return true
+               
+           }        
+    }
+    return false
 }
 
+console.log(SumArray([1,2,3,4], 9));
 module.exports = SumArray;
