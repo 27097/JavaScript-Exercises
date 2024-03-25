bb7e309c91ee3268b3185959f2453e4d4a3b400f
27097 <tapiasgroup@gmail.com>
2024-03-25 12:17:26 -0300

    01

--- 01-FindNeedle/FindNeedle.js
+++ 01-FindNeedle/FindNeedle.js
@@ -1,15 +1,15 @@
 function FindNeedle(haystack, needle) {
-    // Your code here:
-    for (let i = 0; i < haystack.length; i++) {
-        if (haystack[i] === needle[0]) {
-            if (haystack.slice(i, i + needle.length) === needle) return i
-        }
-
+    // iterar sobre el haystack
+    for (let i = 0; i < haystack.length; i += 1) {
+      // si la substring matchea el neddle podemos devoler el valor de i
+      if(haystack[i]===needle[0]){
+        if (haystack.slice(i, i + needle.length) === needle) return i
+      }
     }
-    return -1
+    return -1;
+  }
 
-}
-
+console.log(FindNeedle("react-redux","redux"));
 
 
 module.exports = FindNeedle;
